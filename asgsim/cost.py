031123df4c99636cd4612ce2967e94913b368b9a
Kevin Bell <kevin.a.bell@gmail.com>
2016-03-01 17:28:23 -0800

    Result comparison

--- asgsim/cost.py
+++ asgsim/cost.py
@@ -1,7 +1,19 @@
 import json
+from math import sqrt
 from multiprocessing import Pool
 
+from numpy import mean, std
+
 from .model import run_model
+
+
+# a reasonably average contractor rate multiplied
+# by a bit of a fudge factor for interruption
+COST_PER_DEV_HOUR = 200
+# m4.large on-demand price
+COST_PER_BUILDER_HOUR = 0.12
+# 2x m4.10xl on-demand price
+#COST_PER_BUILDER_HOUR = 4.698
 
 
 def extract_output(model):
@@ -32,15 +44,13 @@ def cost_from_job_results(results):
     sec_per_tick = opts['sec_per_tick']
     cost_per_dev_hour = 100 # a reasonably average contractor rate
     adjusted_cost_per_dev_hour = cost_per_dev_hour * 2 # adjust for a bit of a "concentration loss factor"
-    cost_per_builder_hour = 0.12 # m4.large on-demand price
-    #cost_per_builder_hour = 4.698 # 2x m4.10xl on-demand price
     builds_per_hour = opts['builds_per_hour']
     ticks = opts['ticks']
     simulation_time_hours = ticks * sec_per_tick / 3600.0
 
     def cost_of_output(output):
-        cost_per_hour = (output['mean_unused_builders'] * cost_per_builder_hour
-                         + builds_per_hour * output['mean_queue_time'] / 3600.0 * adjusted_cost_per_dev_hour)
+        cost_per_hour = (output['mean_unused_builders'] * COST_PER_BUILDER_HOUR
+                         + builds_per_hour * output['mean_queue_time'] / 3600.0 * COST_PER_DEV_HOUR)
         return simulation_time_hours * cost_per_hour
 
     if type(output) == list:
@@ -49,7 +59,42 @@ def cost_from_job_results(results):
         return cost_of_output(output)
 
 
-
 def cost(opts):
     results = run_job(opts)
     return cost_from_job_results(results)
+
+
+def cost_ci(results, percent=95):
+    """
+    Returns 95 percent confidence interval for cost of `results`,
+    assuming costs are normally distributed.
+    """
+    assert len(results) > 1
+    costs = cost_from_job_results(results)
+    print costs
+    z = {95: 1.96, 99: 2.58, 99.5: 2.81, 99.9: 3.29} # http://mathworld.wolfram.com/StandardDeviation.html
+    m = mean(costs)
+    s = std(costs)
+    se = s / sqrt(len(costs))
+    return (m - se * z[percent], m + se * z[percent])
+
+
+def compare_cis(ci_a, ci_b):
+    """
+    Returns:
+      1 if cost(a) < cost(b),
+     -1 if cost(b) < cost(a),
+      0 if confidence intervals overlap
+    """
+    if ci_a[1] < ci_b[0]:
+        return 1
+    elif ci_b[1] < ci_a[1]:
+        return -1
+    else:
+        return 0
+
+
+def compare_results(a, b):
+    ci_a = cost_ci(a)
+    ci_b = cost_ci(b)
+    return compare_cis(ci_a, ci_b)
--- tests/test_cost.py
+++ tests/test_cost.py
@@ -1,4 +1,5 @@
-from asgsim.cost import cost
+import asgsim.cost
+from asgsim.cost import cost, cost_ci, compare_cis, compare_results
 
 
 def test_cost_machines():
@@ -11,14 +12,12 @@ def test_cost_machines():
                      'sec_per_tick': 3600,
                      'ticks': 10})
     # build running for one cycle, zero queue time
-    expected = cost_per_builder_hour * 9
-    print 'measured:', measured
-    print 'expected:', expected
+    expected = asgsim.cost.COST_PER_BUILDER_HOUR * 9
     assert measured == expected
+
 
 def test_cost_queueing():
     # TODO: Look into using total queued time to compute cost
-    cost_per_dev_hour = 200
     cost_per_builder_hour = 0.12 # m4.large on-demand price
     measured = cost({'builder_boot_time': 0,
                      'builds_per_hour': 1.0,
@@ -29,7 +28,41 @@ def test_cost_queueing():
                      'ticks': 4})
     # 3 builds will finish, no free machines, queue times are 0, 1, 2 hrs
     mean_queue_time = (0 + 1 + 2) / 3.0
-    expected = cost_per_dev_hour * mean_queue_time * 4
-    print 'measured:', measured
-    print 'expected:', expected
+    expected = asgsim.cost.COST_PER_DEV_HOUR * mean_queue_time * 4
     assert measured == expected
+
+
+def test_cost_ci():
+    actual = cost_ci({'input': {'sec_per_tick': 3600, 'builds_per_hour': 1, 'ticks': 1},
+                      'output': [{'mean_queue_time': 0, 'mean_unused_builders': 1},
+                                 {'mean_queue_time': 0, 'mean_unused_builders': 2},
+                                 {'mean_queue_time': 0, 'mean_unused_builders': 3},
+                                 {'mean_queue_time': 0, 'mean_unused_builders': 4}]})
+    # costs = 0.12 * [1, 2, 3, 4] = [0.12, 0.24, 0.36, 0.48]
+    # m = mean([0.12, 0.24, 0.36, 0.48]) = 0.29999999999999999
+    # s = std([0.12, 0.24, 0.36, 0.48]) = 0.13416407864998739
+    # se = s / sqrt(4) = 0.13416407864998739 / 2 = 0.0670820393249937
+    # z = 1.96
+    # lower = 0.29999999999999999 - 0.0670820393249937 * 1.96 = 0.16851920292301234
+    # upper = 0.29999999999999999 + 0.0670820393249937 * 1.96 = 0.4314807970769876
+    expected = (0.16851920292301234, 0.4314807970769876)
+    assert actual == expected
+
+
+def test_compare_cis():
+    ci_a = (1, 5)
+    ci_b = (4, 6)
+    ci_c = (6, 9)
+    assert compare_cis(ci_a, ci_b) == 0
+    assert compare_cis(ci_a, ci_c) == 1
+    assert compare_cis(ci_c, ci_a) == -1
+
+
+def test_compare_results():
+    bad = {'input': {'sec_per_tick': 3600, 'builds_per_hour': 1, 'ticks': 1},
+           'output': [{'mean_queue_time': 0, 'mean_unused_builders': 5000},
+                      {'mean_queue_time': 0, 'mean_unused_builders': 5000}]}
+    good = {'input': {'sec_per_tick': 3600, 'builds_per_hour': 1, 'ticks': 1},
+           'output': [{'mean_queue_time': 0, 'mean_unused_builders': 1},
+                      {'mean_queue_time': 0, 'mean_unused_builders': 1}]}
+    assert compare_results(good, bad) == 1
