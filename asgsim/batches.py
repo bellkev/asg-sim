425f27935910eb1ae5e2731c982183cd64605fcc
Kevin Bell <kevin.a.bell@gmail.com>
2016-03-01 17:53:19 -0800

    Reorganize batches and add static fleet batches

--- asgsim/batches.py
+++ asgsim/batches.py
@@ -3,12 +3,16 @@ import os
 import sys
 from multiprocessing import Pool
 
-from ..cost import cost, run_job
+from .cost import cost, run_job
 
 
-HIGH_RESOLUTION=10
-LOW_RESOLUTION=60
-TRIAL_DURATION_SECS=100000 # about a day
+HIGH_RESOLUTION = 10
+LOW_RESOLUTION = 60
+TRIAL_DURATION_SECS = 100000 # about a day
+
+# Determined from asgsim.plots.static methods
+STATIC_MINIMA = [(300, 10.0, 5), (300, 50.0, 12), (300, 200.0, 31),
+                 (60, 50.0, 5), (120, 50.0, 7), (600, 50.0, 19), (1200, 50.0, 31)]
 
 
 def sec_per_tick(*times):
@@ -18,11 +22,37 @@ def sec_per_tick(*times):
         return LOW_RESOLUTION
 
 
-def generate_jobs(path):
-
+def generate_jobs(jobs, path):
     jobs_per_batch = 100
-    up_down_range = [1, 2, 4, 8, 16, 32]
+    for job in jobs:
+        job['sec_per_tick'] = sec_per_tick(job['build_run_time'],
+                                           job.get('builder_boot_time', 9999),
+                                           job.get('alarm_period_duration', 9999))
+        job['ticks'] = TRIAL_DURATION_SECS / job['sec_per_tick']
+    batch_count = len(jobs) / jobs_per_batch + 1
+    for batch in range(batch_count):
+        start = batch * jobs_per_batch
+        end = start + jobs_per_batch
+        batch_jobs = jobs[start:end]
+        in_dir = os.path.join(path, 'input')
+        if not os.path.isdir(in_dir):
+            os.mkdir(in_dir)
+        with open(os.path.join(in_dir, '%04d' % batch), 'w') as batch_file:
+            json.dump(batch_jobs, batch_file)
 
+
+def generate_static_jobs(path):
+    jobs = [{'autoscale': False,
+             'trials': 20,
+             'build_run_time': build_time,
+             'builds_per_hour': traffic,
+             'initial_builder_count': initial}
+            for build_time, traffic, initial in STATIC_MINIMA]
+    generate_jobs(jobs, path)
+
+
+def generate_autoscaling_jobs(path):
+    up_down_range = [1, 2, 4, 8, 16, 32]
     # Whee! List comprehension!
     jobs = [{'autoscale': True,
              'trials': 5,
@@ -35,11 +65,9 @@ def generate_jobs(path):
              'scale_up_threshold': up_threshold,
              'scale_down_threshold': down_threshold,
              'scale_up_change': scale_up_change,
-             'scale_down_change': scale_down_change,
-             'sec_per_tick': sec_per_tick(build_time, boot_time, alarm_period_duration)}
+             'scale_down_change': scale_down_change}
             # Start at optimum static fleet sizes
-            for build_time, traffic, initial in [(300, 10.0, 5), (300, 50.0, 12), (300, 200.0, 31),
-                                                 (60, 50.0, 5), (120, 50.0, 7), (600, 50.0, 19), (1200, 50.0, 31)]
+            for build_time, traffic, initial in STATIC_MINIMA
             for boot_time in [10, 30, 60, 120, 300, 600, 1200]
             for alarm_period_duration in [10, 60, 300]
             for alarm_period_count in [1, 2, 4]
@@ -47,20 +75,7 @@ def generate_jobs(path):
             for up_threshold, down_threshold in [(up, down) for up in up_down_range for down in up_down_range if up <= down]
             for scale_up_change in [1, 2, 4]
             for scale_down_change in [1, 2, 4]]
-
-    for job in jobs:
-            job['ticks'] = TRIAL_DURATION_SECS / job['sec_per_tick']
-
-    batch_count = len(jobs) / jobs_per_batch + 1
-    for batch in range(batch_count):
-        start = batch * jobs_per_batch
-        end = start + jobs_per_batch
-        batch_jobs = jobs[start:end]
-        in_dir = os.path.join(path, 'input')
-        if not os.path.isdir(in_dir):
-            os.mkdir(in_dir)
-        with open(os.path.join(in_dir, '%04d' % batch), 'w') as batch_file:
-            json.dump(batch_jobs, batch_file)
+    generate_jobs(jobs, path)
 
 
 def run_batch(path, batch_name, procs=6):
@@ -92,7 +107,7 @@ def run_batches(path, **kwargs):
 
 
 if __name__ == '__main__':
-    usage = 'Usage: python -m asgsim.plots.autoscaling <generate|run> path [procs]'
+    usage = 'Usage: python -m asgsim.batches <generate-auto|generate-static|run> path [procs]'
     if len(sys.argv) < 3:
         print usage
         exit(1)
@@ -101,9 +116,12 @@ if __name__ == '__main__':
     procs = 6
     if len(sys.argv) == 4:
         procs = int(sys.argv[3])
-    if sys.argv[1] == 'generate':
-        print 'Generating jobs in', path
-        generate_jobs(sys.argv[2])
+    if sys.argv[1] == 'generate-auto':
+        print 'Generating autoscaling jobs in', path
+        generate_autoscaling_jobs(sys.argv[2])
+    elif sys.argv[1] == 'generate-static':
+        print 'Generating static jobs in', path
+        generate_static_jobs(sys.argv[2])
     elif sys.argv[1] == 'run':
         print 'Running jobs in %s with %d processes' % (path, procs)
         run_batches(sys.argv[2], procs=procs)
