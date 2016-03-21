a37df8a70bc33121a7018934db38dcc379f357d0
Kevin Bell <kevin.a.bell@gmail.com>
2016-03-20 22:49:03 -0700

    Test autoscaling parameter recommendations

--- asgsim/plots/autoscaling.py
+++ asgsim/plots/autoscaling.py
@@ -1,12 +1,13 @@
 from collections import defaultdict
 import csv
-from math import log
+from math import log, sqrt
+from multiprocessing import Pool
 
 from numpy import mean
 
-from ..batches import generate_jobs, load_results, STATIC_MINIMA, STATIC_MINIMA_LIMITED, BOOT_TIMES, TRIAL_DURATION_SECS
-from ..cost import costs_from_job_results, cost_ci, compare_result_means, COST_PER_BUILDER_HOUR_EXPENSIVE
-from ..model import run_model
+from ..batches import generate_jobs, load_results, STATIC_MINIMA, STATIC_MINIMA_LIMITED, BOOT_TIMES
+from ..cost import costs, run_job, costs_from_job_results, cost_ci, compare_result_means, COST_PER_BUILDER_HOUR_EXPENSIVE
+from ..model import Model, run_model
 from .utils import plt, plt_title, plt_save, make_scaling_plot
 
 
@@ -35,14 +36,20 @@ def param_match_pred(d):
     return lambda x: param_match(d, x)
 
 
+def auto_key_fn(x):
+    return (x['build_run_time'], x['builds_per_hour'], x['builder_boot_time'])
+
+
+def static_key_fn(x):
+    return (x['build_run_time'], x['builds_per_hour'])
+
+
 def min_auto_params(static, auto, **kwargs):
     """
     Given static and autoscaling results, return the parameters
     that minimize cost for each set of independent variables,
     with savings over static fleets added into the param dict.
     """
-    auto_key_fn = lambda x: (x['build_run_time'], x['builds_per_hour'], x['builder_boot_time'])
-    static_key_fn = lambda x: (x['build_run_time'], x['builds_per_hour'])
     static_costs = {}
     min_auto_costs = {}
     min_autos = {}
@@ -174,21 +181,108 @@ def make_constant_traffic_plots():
     make_savings_v_traffic_plot(load_results('job-archives/2c517e8/static'), load_results('job-archives/2c517e8/candidates2'))
     make_savings_v_boot_time_plot(load_results('job-archives/2c517e8/static'), load_results('job-archives/2c517e8/candidates2'))
     make_savings_v_dev_cost_plot(load_results('job-archives/2c517e8/static-expensive'), load_results('job-archives/2c517e8/auto'))
-
-
-if __name__ == '__main__':
-    make_constant_traffic_plots()
     params = one_min_auto_params(load_results('job-archives/2c517e8/static'), load_results('job-archives/2c517e8/candidates2'),
                                           {'build_run_time': 2400, 'builds_per_hour': 2, 'builder_boot_time': 300})
     params['ticks'] = 2000
-    params2 = one_min_auto_params(load_results('jobs/static'), load_results('jobs/candidates1'),
-                                          {'build_run_time': 300, 'builds_per_hour': 200.0, 'builder_boot_time': 300})
-    params2['ticks'] = 1500 * 60
-    print params2
-    make_scaling_plot(params,
-                      'Auto Scaling Fleet Capacity and Usage (40 min / build, 2 builds / hr)', 'plots/slow_auto_scaling', axis=[0, 20000 / 60, 0, 8])
-    make_scaling_plot(params2,
-                      'Auto Scaling Fleet Capacity and Usage (5 min / build, 200 builds / hr)', 'plots/sine_auto_scaling', axis=[0, 1500, 0, 35])
+    make_scaling_plot(params, 'Auto Scaling Fleet Capacity and Usage (40 min / build, 2 builds / hr)',
+                      'plots/slow_auto_scaling', axis=[0, 20000 / 60, 0, 8])
 
-    # make_savings_v_traffic_plot_varying(load_results('job-archives/2c517e8/static'), load_results('job-archives/2c517e8/candidates2'),
-    #                                     load_results('jobs/static'), load_results('jobs/candidates1'))
+
+def make_varying_traffic_plots():
+    make_savings_v_traffic_plot_varying(load_results('job-archives/2c517e8/static'), load_results('job-archives/2c517e8/candidates2'),
+                                        load_results('job-archives/9987101/static'), load_results('job-archives/9987101/candidates2'))
+    params = one_min_auto_params(load_results('job-archives/9987101/static'), load_results('job-archives/9987101/candidates2'),
+                                  {'build_run_time': 300, 'builds_per_hour': 200.0, 'builder_boot_time': 300})
+    params['ticks'] = 1500 * 60
+    make_scaling_plot(params, 'Auto Scaling Fleet Capacity and Usage (5 min / build, 200 builds / hr)',
+                      'plots/sine_auto_scaling', axis=[0, 1500, 0, 35])
+
+
+def compute_params(params):
+    builds_per_hour = params['builds_per_hour']
+    build_run_time = params['build_run_time']
+    volume = builds_per_hour * build_run_time / 3600.0
+    desired_capacity = 1.17 + 2.52 * sqrt(volume) # results of sqrt regression
+    hysteresis = desired_capacity * 0.2
+    scale_up_threshold = int(round(desired_capacity - hysteresis))
+    scale_down_threshold = int(round(desired_capacity + hysteresis))
+    scale_up_change = max(1, int(round(desired_capacity * 0.4)))
+    scale_down_change = max(1, int(round(desired_capacity * 0.2)))
+    fmt_str = 'up_thresh: %d down_thresh: %d up_change: %d down_change: %d'
+    return {'autoscale': True,
+            'builds_per_hour': builds_per_hour,
+            'builds_per_hour_fn': params['builds_per_hour_fn'],
+            'build_run_time': build_run_time,
+            'builder_boot_time': params['builder_boot_time'],
+            'alarm_period_duration': 300,
+            'scale_up_alarm_period_count': 1,
+            'scale_down_alarm_period_count': 2,
+            'scale_up_change': scale_up_change,
+            'scale_down_change': scale_down_change,
+            'scale_up_threshold': scale_up_threshold,
+            'scale_down_threshold': scale_down_threshold,
+            'initial_builder_count': scale_up_threshold,
+            'sec_per_tick': params['sec_per_tick'],
+            'ticks': params['ticks']}
+
+
+def non_autoscaling_params(params):
+    optimum_fleet_sizes = {(30, 50.0): 4,
+                           (60, 50.0): 5,
+                           (120, 50.0): 7,
+                           (300, 50.0): 12,
+                           (600, 50.0): 19,
+                           (1200, 50.0): 31,
+                           (2400, 50.0): 55,
+                           (300, 10.0): 5,
+                           (300, 50.0): 12,
+                           (300, 100.0): 19,
+                           (300, 200.0): 31,
+                           (300, 500.0): 63,
+                           (300, 1000.0): 114}
+    ret = params.copy()
+    ret['autoscale'] = False
+    ret['initial_builder_count'] = optimum_fleet_sizes[static_key_fn(params)]
+    return ret
+
+
+def test_recommendations():
+    static_results = load_results('job-archives/9987101/static')
+    auto_results = load_results('job-archives/9987101/candidates2')
+    experimental_params = [p for p in min_auto_params(static_results, auto_results) if p['builder_boot_time'] == 300]
+    recommended_params = map(compute_params, experimental_params)
+    static_params = map(non_autoscaling_params, experimental_params)
+    # Add some high-traffic situations that weren't covered by batch runs
+    extra_params = [{'build_run_time': run_time, 'builds_per_hour': per_hour,
+                     'builds_per_hour_fn': Model.SINE, 'builder_boot_time': boot_time,
+                     'ticks': 20000, 'sec_per_tick': 10}
+                    for run_time, per_hour in [(2400, 50.0), (300, 500.0), (300, 1000.0)]
+                    for boot_time in [300]]
+    recommended_params.extend(map(compute_params, extra_params))
+    static_params.extend(map(non_autoscaling_params, extra_params))
+    percent_savings = lambda a, s: int(round((1 - a / s) * 100))
+    p = Pool(8)
+    for e, r, s in zip(experimental_params, recommended_params, static_params):
+        e['trials'] = r['trials'] = s['trials'] = 50
+    experimental_costs = map(mean, p.map(costs, experimental_params))
+    recommended_costs = map(mean, p.map(costs, recommended_params))
+    static_costs = map(mean, p.map(costs, static_params))
+    p.close()
+    volumes = [p['builds_per_hour'] * p['build_run_time'] / 3600.0 for p in static_params]
+    experimental_savings = [percent_savings(e, s) for e, s in zip(experimental_costs, static_costs)]
+    recommended_savings = [percent_savings(r, s) for r, s in zip(recommended_costs, static_costs)]
+    fmt_str = 'builder_boot_time: %d build_run_time: %d builds_per_hour: %.0f recommended_savings: %d'
+    plt_title('Savings Over Static Fleet vs Build Volume (5 min boot times)')
+    plt.xlabel('Volume (build hrs / hr) (erlangs)')
+    plt.ylabel('Savings (%)')
+    e_handle, = plt.plot(volumes[:len(experimental_savings)], experimental_savings, 'bo', label='Best parameters measured')
+    r_handle, = plt.plot(volumes, recommended_savings, 'gs', label='Parameters from formula')
+    plt.legend(handles=(e_handle, r_handle), loc='lower right')
+    plt.axis([0, 90, 0, 65])
+    plt_save('plots/test_recommendations')
+
+
+if __name__ == '__main__':
+    # make_constant_traffic_plots()
+    # make_varying_traffic_plots()
+    test_recommendations()
